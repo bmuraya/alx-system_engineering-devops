5d385ffd996f730ebe661be1dfe66d0b0c3d3cac
bmuraya <babumuraya@gmail.com>
2023-10-21 09:23:45 +0300

    puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,9 +1,27 @@
 # Puppet manifest to install nginx
 
-exec {'install':
+exec { 'install_nginx':
   provider => shell,
-  command  => 'sudo apt-get -y update; sudo apt-get -y install nginx;\
-  echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html;\
-  "s/server_name _;server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/bmuraya permanent;/" /etc/nginx/sites-available/default;\
-  sudo service nginx start',
+  command  => 'sudo apt-get -y update; sudo apt-get -y install nginx; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html',
 }
+
+file_line { 'nginx_redirect_rule':
+  path    => '/etc/nginx/sites-available/default',
+  line    => 'rewrite ^/redirect_me https://github.com/bmuraya permanent;',
+  match   => 'server_name _;',
+  ensure  => present,
+  require => Exec['install_nginx'],
+  notify  => Service['nginx'],
+}
+
+service { 'nginx':
+  ensure    => 'running',
+  enable    => true,
+  require   => File_line['nginx_redirect_rule'],
+}
+
+exec { 'restart_nginx':
+  command  => 'sudo service nginx restart',
+  require  => File_line['nginx_redirect_rule'],
+}
+
